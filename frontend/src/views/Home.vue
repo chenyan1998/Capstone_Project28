de8ff23454d19f782f1f35cec1a0e9a4f47fbf0d
Erasmo1015 <2675325968@qq.com>
2021-05-20 23:43:17 +0800

    Initialize file structure

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div v-for="nav in nav_list" :key="nav">
+      <router-link :to="{name: nav}">
+            <button>{{nav}}</button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  name: 'Home',
+  setup(){
+    const nav_list = ref(['ReportHomepage','SurveyHomepage'])
+    console.log(nav_list.value)
+    return {nav_list}
+  }
+}
+</script>
