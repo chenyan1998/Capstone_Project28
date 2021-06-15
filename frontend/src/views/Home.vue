1e374c05544b3c4b032b35babbace5444e280795
apr1l4nn <82123187+apr1l4nn@users.noreply.github.com>
2021-06-15 17:52:36 +0800

    [CAP-14]

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
+<html>
+  <TopNavigationBar/>
   <div class="home">
+    
     <Sidebar :current_path="1" />
     <!-- <div v-for="nav in nav_list" :key="nav">
       <router-link :to="{name: nav}">
@@ -7,13 +10,18 @@
       </router-link>
     </div> -->
   </div>
+  <h1> This is Home Page </h1>
+</html>
 </template>
 <script>
 import Sidebar from '../components/Sidebar'
 import {ref} from 'vue'
+import TopNavigationBar from '../components/TopNavigationBar.vue'
+
+
 export default {
   name: 'Home',
-  components: {Sidebar},
+  components: {Sidebar, TopNavigationBar},
   setup(){
     const nav_list = ref(['Profile','ReportHomepage','SurveyHomepage'])
     return {nav_list}
