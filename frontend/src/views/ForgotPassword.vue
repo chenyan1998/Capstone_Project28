e6db7ef4aba481251484d05cc78bc22b9877a421
apr1l4nn <82123187+apr1l4nn@users.noreply.github.com>
2021-06-15 17:53:21 +0800

    [CAP-5]

--- frontend/src/views/ForgotPassword.vue
+++ frontend/src/views/ForgotPassword.vue
@@ -1,5 +1,27 @@
 <template>
-  <h2>New Title</h2>
+<html>
+<div class="container">
+  <img src="@/assets/resetpassword.png" alt="Snow" style="width:100%;">
+  <div class="centered">
+    <h3>Note : If you continue, DB Schenker will send a link to your company email address. Click the link in the message, and enter a new password on the page that opens.
+      </h3>
+    <div class="resetpassword1">
+  <form method="post" action="">
+    <p><input type="text" name="login" value="" placeholder="Username or Email"></p>
+    <br>
+    <p class="submit"><input type="submit" name="commit" value="Reset Password"></p>
+  </form>
+  
+  
+  </div>
+  
+    <div id = "nav4">
+    <router-link to ="/"> Back to Login Page </router-link>
+    </div>
+    <router-view/>
+  </div>
+</div>
+</html>
 </template>
 
 <script>
@@ -9,5 +31,43 @@ export default {
 </script>
 
 <style>
+.container {
+  position: relative;
+  text-align: center;
+  color: white;
+}
+.top-left {
+  position: absolute;
+  top: 80px;
+  left: 20px;
+}
 
+.top-left2 {
+  position: absolute;
+  top: 200px;
+  left: 350px;
+}
+
+
+.html {
+  height: 100%;
+}
+
+.centered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: black;
+}
+
+.submit {
+  text-align : right
+
+}
+
+#nav4 {
+  text-align : right
+
+}
 </style>
